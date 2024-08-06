5ca47a919120454c1feedb3593b6f39d5cf6a75e
𝙰𝚍𝚊𝚖 <82734867+Pixelisteuhh@users.noreply.github.com>
2024-08-06 15:07:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,40 +27,45 @@ const client = new Discord.Client({
         Discord.Intents.FLAGS.GUILD_VOICE_STATES,
         Discord.Intents.FLAGS.GUILD_WEBHOOKS,
     ]
-});
-
-client.commands = new Discord.Collection();
-
-login(client);
-
+})
+const {
+	readdirSync
+} = require("fs")
+const db = require('quick.db')
+const ms = require("ms")
+const {
+	MessageEmbed
+} = require('discord.js')
+const {
+	login
+} = require("./util/login.js");
+login(client)
 process.on("unhandledRejection", err => {
-    if (err.message) return;
-    console.error("Uncaught Promise Error: ", err);
-});
-
+	if (err.message) return
+	console.error("Uncaught Promise Error: ", err);
+})
 const loadCommands = (dir = "./commands/") => {
-    readdirSync(dir).forEach(dirs => {
-        const commands = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"));
+	readdirSync(dir).forEach(dirs => {
+		const commands = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"));
 
-        for (const file of commands) {
-            const getFileName = require(`${dir}/${dirs}/${file}`);
-            client.commands.set(getFileName.name, getFileName);
-            console.log(`> Commande Chargée ${getFileName.name} [${dirs}]`);
-        }
-    });
+		for (const file of commands) {
+			const getFileName = require(`${dir}/${dirs}/${file}`);
+			client.commands.set(getFileName.name, getFileName);
+			console.log(`> Commande Charger ${getFileName.name} [${dirs}]`)
+		};
+	});
 };
-
 const loadEvents = (dir = "./events/") => {
-    readdirSync(dir).forEach(dirs => {
-        const events = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"));
+	readdirSync(dir).forEach(dirs => {
+		const events = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"));
 
-        for (const event of events) {
-            const evt = require(`${dir}/${dirs}/${event}`);
-            const evtName = event.split(".")[0];
-            client.on(evtName, evt.bind(null, client));
-            console.log(`> Event Chargé ${evtName}`);
-        }
-    });
+		for (const event of events) {
+			const evt = require(`${dir}/${dirs}/${event}`);
+			const evtName = event.split(".")[0];
+			client.on(evtName, evt.bind(null, client));
+			console.log(`> Event Charger ${evtName}`)
+		};
+	});
 };
 
 loadEvents();
